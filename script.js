c903539921395995c1eaf38af817267b55199784
Nijel13 <70838712+Nijel13@users.noreply.github.com>
2021-05-29 18:05:02 +0300

    lesson2, work with variables and strings

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-let money;
-let income;
-let addExpenses; 
-let deposit;
-let mission; 
-let period;
-alert("Hello, world");
-console.log('test console');
+const money = 100500;
+const income = "freelance";
+const addExpenses = "Mobile, Ethernet, Food, Games, Study, CHILL";
+const deposit = true;
+const mission = 1500000;
+const period = 12;
+
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+console.log(addExpenses.length);
+console.log("\"Период равен " + period + " месяцев\"");
+console.log("\"Цель заработать " + mission + " рублей\"");
+console.log(addExpenses.toLowerCase().split(", "));
+
+const budgetDay = money/30;
+console.log(budgetDay);
